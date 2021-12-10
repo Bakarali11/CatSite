e43157d65f9dd6da2ca9576b17f5a7d923ac721f
KarlSherman1999 <karlsherman1999@gmail.com>
2021-12-10 12:01:56 +0000

    4

--- src/catSale.js
+++ src/catSale.js
@@ -1,4 +1,7 @@
 import { useState } from "react";
+import img1 from './cat1.jpg';
+import img2 from './cat2.jpg';
+import img3 from './cat3.jpg';
 import { useEffect } from "react";
 import React from "react";
 import {
@@ -8,6 +11,7 @@ Route,
 Link
 } from "react-router-dom";
 const faker = require('faker');
+
 
 const App = () =>{
     const [basket,setBasket]=useState([])
@@ -55,7 +59,11 @@ const App = () =>{
     }
     
     function About() {
-        return <h1 className="header">About</h1>;
+        return (<div>
+        <h1 className="header">About</h1>
+        <Aboutt/>
+        </div>
+        )
     }
     
     function User() {
@@ -73,6 +81,7 @@ const App = () =>{
                 <h1>Basket</h1>
                 </div>
             <Basket basket={basket} setBasket={setBasket}/>
+            <h2>Total: £</h2>
             </div>
         )
     }
@@ -87,7 +96,7 @@ const App = () =>{
             <Link to="/">Home</Link>
             </li>
             <li>
-            <Link to="/about">About</Link>
+            <Link to="/About">About</Link>
             </li>
             <li>
             <Link to="/User">Cats for sale</Link>
@@ -98,7 +107,7 @@ const App = () =>{
         </ul>
         </nav>
         <Switch>
-        <Route path="/about">
+        <Route path="/About">
             <About />
         </Route>
         <Route path="/User">
@@ -136,7 +145,7 @@ const Cat = ({sale, setBasket, basket}) => {
                 <img key={index} src={cat.url}/>
                 <h2>{cat.name}</h2>
                 <h2>Price: £{cat.price}</h2>
-                <button onClick={ () => updateBasket(cat)}>Add to basket</button>
+                <button onClick={ () => {updateBasket(cat)}}>Add to basket</button>
                 </div>
             )
             })} 
@@ -164,6 +173,28 @@ return(
     </div> )
 }
 
+const Aboutt = () => {
+    return <div>
+        {/* <h1>About Us</h1> */}
+        <p>Hello we are cats4lyf</p>
+        <br/>
+        <p>We're a cat sales agency that help businesses drive revenue with the use of inbound marketing and sales. Our team is made up of smart and talented people that are passionate about creating inbound sales results.</p>
+        <br />
+        <p>In 2021, we spoke to over 10,000 people from lifelong cat-lovers to potential owners.</p>
+        <br />
+        <p>Our vision is a world where every cat is treated with kindness and an understanding of it's needs. Yes we know its an ambitious vision. However, we truly believe it can be achieved because of our passion, our professional approach and simply because cats are among the UK's most popular companion pets.</p>
+        <br />
+        </div>
+        function images() {
+            return(
+                <div>
+                <img src={img1} height={200} width={200} alt=''/>
+                <img src={img2} height={200} width={200} alt='' />
+                <img src={img3} height={200} width={200} alt='' />
+                </div>
+            )
+        }
+    }
 
 
 export default App;
